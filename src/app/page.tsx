b10a76ed54e72734676132744877581357a0492a
Isti <istvan.pasztor@partners.neste.com>
2023-05-18 18:37:54 +0300

    update main page (tempoary)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
-import { Posts } from "@/components";
+"use client";
+
+import { Auth } from "@/components";
+import { useAuthContext } from "@/context/AuthContext";
+import Link from "next/link";
 
 export default function Home() {
+  const { user } = useAuthContext();
+
   return (
-    <main className="w-full h-screen bg-slate-50">
+    <main className="w-full h-screen bg-slate-50 text-center">
       <div className="p-10">
-        <h1 className="text-center font-semibold text-sm">This is App</h1>
+        <h1 className="font-semibold text-sm">This is App</h1>
       </div>
-      <div className="text-center">
-        <Posts />
+
+      <div className="my-10 mb-16">
+        {!user ? <Auth /> : "You are logged in"}
+      </div>
+
+      <div>
+        <Link href="auth" className="text-sky-500 underline font-bold">
+          Go to auth »
+        </Link>
       </div>
     </main>
   );
