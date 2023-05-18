ab7a4604a0671798a756436028380f571f663025
Isti <istvan.pasztor@partners.neste.com>
2023-05-18 18:37:06 +0300

    create Auth compo

---
+++ src/components/Auth.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import signUp from "@/utils/signUp";
+
+export default function Auth() {
+  return (
+    <div className="text-center">
+      <br />
+      <h1 className="text-xs">Please authenticate to continue:</h1>
+
+      <button
+        className="m-2 px-4 py-2 bg-sky-500 text-white text-sm font-bold rounded-md"
+        onClick={() => signUp()}
+      >
+        LOGIN WITH GOOGLE
+      </button>
+    </div>
+  );
+}
