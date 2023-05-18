5e9cf1bdb423de418f32d980a5c281017c28e7bf
Isti <istvan.pasztor@partners.neste.com>
2023-05-18 16:23:40 +0300

    add test component to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
+import { Posts } from "@/components";
 
 export default function Home() {
   return (
     <main className="w-full h-screen bg-slate-50">
       <div className="p-10">
         <h1 className="text-center font-semibold text-sm">This is App</h1>
+      </div>
+      <div className="text-center">
+        <Posts />
       </div>
     </main>
   );
