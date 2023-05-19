4237e7e3727d1c29b40f55c2ca7684cae4278752
Istip <isticsek@gmail.com>
2023-05-19 21:46:29 +0300

    create temporary Navbar compo

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useAuthContext } from "@/context/AuthContext";
+import Link from "next/link";
+import Auth from "./Auth";
+import Image from "next/image";
+
+export default function Navbar() {
+  const { user } = useAuthContext();
+
+  return (
+    <nav className="w-screen h-12 bg-slate-800 border-b border-slate-950 px-4 fixed flex justify-center">
+      <div className="w-full max-w-7xl flex justify-between items-center gap-4">
+        <div>
+          <Link href="/" className="font-bold text-xs text-yellow-500">
+            POSTBOARD
+          </Link>
+        </div>
+        <div>
+          {user ? (
+            <div className="flex items-center gap-4">
+              <button className="text-xs font-bold px-4 py-2 rounded-md bg-yellow-500 text-slate-950 hover:bg-yellow-600 transition-all">
+                LOGOUT
+              </button>
+              <Image
+                className="rounded-full text-xs border border-yellow-500"
+                src={user?.photoURL ?? "/default_avatar.bmp"}
+                width={32}
+                height={32}
+                alt={user?.displayName ?? "User"}
+              />
+            </div>
+          ) : (
+            <Auth />
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+}
