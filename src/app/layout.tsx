f61cde4c4c59d525efb258138a46b8d056ae53ac
Istip <isticsek@gmail.com>
2023-05-19 22:27:32 +0300

    update main layout with new changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { AuthContextProvider } from "@/context/AuthContext";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="bg-slate-900 text-slate-50 w-full h-screen">
+        <div className="bg-slate-950 text-slate-50 w-full min-h-screen h-auto">
           <AuthContextProvider>
             <Navbar />
-            <div className="pt-12">{children}</div>
+            <div className="py-16 px-4">{children}</div>
+            <Footer />
           </AuthContextProvider>
         </div>
       </body>
