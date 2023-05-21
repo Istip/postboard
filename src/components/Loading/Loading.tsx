17d992004fb9c39626cc5fd023f13cac2b064e56
Istip <isticsek@gmail.com>
2023-05-21 13:24:47 +0300

    update Loading component to work with and without loading text too

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,13 +1,38 @@
+import React from "react";
 import styles from "./loading.module.css";
 
-export default function Loading() {
-  return (
-    <div className="w-100 h-96 overflow-hidden flex flex-wrap flex-col items-center justify-center">
-      <div className="text-yellow-500 font-bold text-2xl">POSTBOARD</div>
+const Loading: React.FC<{ text?: boolean }> = ({ text = true }) => {
+  const WrapperText: React.FC<{ children: React.ReactNode }> = ({
+    children,
+  }) => {
+    if (!text) {
+      return <>{children}</>;
+    }
+
+    return (
+      <>
+        <div className="text-yellow-500 font-bold text-2xl">POSTBOARD</div>
+        {children}
+        <div className="text-yellow-500 font-bold text-sm">Loading</div>
+      </>
+    );
+  };
+
+  const Spinner = () => {
+    return (
       <div className={styles.loading}>
         <div />
       </div>
-      <div className="text-yellow-500 font-bold text-sm">Loading</div>
+    );
+  };
+
+  return (
+    <div className="w-100 overflow-hidden flex flex-wrap flex-col items-center justify-center">
+      <WrapperText>
+        <Spinner />
+      </WrapperText>
     </div>
   );
-}
+};
+
+export default Loading;
