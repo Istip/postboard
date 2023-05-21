14aefe053fc5be19c3cf5ea728314003a8b4f536
Istip <isticsek@gmail.com>
2023-05-21 22:54:11 +0300

    fix layout according to viewports

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 const PageLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <section className="px-4 pt-16 pb-36">{children}</section>;
+  return (
+    <section className="px-4 pt-16 pb-36 container m-auto max-w-7xl lg:px-2">
+      <div className="">{children}</div>
+    </section>
+  );
 };
 export default PageLayout;
