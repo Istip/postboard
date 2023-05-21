02a1477f5e73fcfaa8ce952ae3bc9cf9f9ea9962
Istip <isticsek@gmail.com>
2023-05-21 20:47:23 +0300

    improve file structure and imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import { AuthContextProvider } from "@/context/AuthContext";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
