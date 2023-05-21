678a5b10b3500567ced5ebc2060c38cf777790ca
Istip <isticsek@gmail.com>
2023-05-21 14:45:25 +0300

    add input field for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useAuthContext } from "@/context/AuthContext";
+import { PlusCircledIcon } from "@radix-ui/react-icons";
 
 export default function Footer() {
   const { user } = useAuthContext();
@@ -10,15 +11,26 @@ export default function Footer() {
   }
 
   return (
-    <div className="w-screen h-12 bg-slate-900 border-t border-slate-800 px-4 fixed flex justify-center bottom-0">
-      <div className="w-full max-w-7xl flex justify-between items-center gap-4">
-        <button className="font-bold text-xs bg-yellow-500 px-4 py-2 rounded-md text-slate-950">
-          SHOPPING
+    <div className="w-screen bg-slate-900 border-slate-800 border-t fixed bottom-0">
+      <div className="px-4 py-2 h-16 flex gap-2">
+        <textarea
+          placeholder="Enter your text..."
+          className="w-full h-full bg-slate-800 p-2 rounded-md border-transparent border focus:outline-none focus:border focus:border-slate-800 resize-none text-sm"
+        />
+        <button className="h-full bg-yellow-500 rounded-md px-4 py-2 text-slate-950 font-bold">
+          <PlusCircledIcon />
         </button>
-        <button className="font-bold text-xs text-yellow-500">NOTES</button>
-        <button className="font-bold text-xs text-yellow-500">
-          NOTIFICATIONS
-        </button>
+      </div>
+      <div className="px-4 flex justify-center">
+        <div className="max-w-7xl flex justify-between items-center gap-4 w-96 py-4">
+          <button className="font-bold text-xs bg-yellow-500 px-4 py-2 rounded-md text-slate-950">
+            SHOPPING
+          </button>
+          <button className="font-bold text-xs text-yellow-500">NOTES</button>
+          <button className="font-bold text-xs text-yellow-500">
+            NOTIFICATIONS
+          </button>
+        </div>
       </div>
     </div>
   );
