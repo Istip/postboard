c18743682ade606d8106eb3d7491746866389c4a
Istip <isticsek@gmail.com>
2023-05-21 16:41:56 +0300

    update footer look

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,38 +1,81 @@
 "use client";
 
 import { useAuthContext } from "@/context/AuthContext";
-import { PlusCircledIcon } from "@radix-ui/react-icons";
+import {
+  PlusCircledIcon,
+  ListBulletIcon,
+  ChatBubbleIcon,
+  BellIcon,
+} from "@radix-ui/react-icons";
+import { usePathname } from "next/navigation";
 
 export default function Footer() {
   const { user } = useAuthContext();
+
+  const pathname = usePathname();
+
+  const menu = [
+    {
+      title: "shopping",
+      icon: <ListBulletIcon />,
+      bagde: null,
+    },
+    {
+      title: "notes",
+      icon: <ChatBubbleIcon />,
+      bagde: null,
+    },
+    {
+      title: "notifications",
+      icon: <BellIcon />,
+      bagde: 10,
+    },
+  ];
 
   if (!user) {
     return null;
   }
 
   return (
-    <div className="w-screen bg-slate-900 border-slate-800 border-t fixed bottom-0">
-      <div className="w-full px-4 py-2 h-16 flex gap-2">
-        <textarea
-          placeholder="Enter your text..."
-          className="w-full h-full bg-slate-800 p-2 rounded-md border-transparent border focus:outline-none focus:border focus:border-slate-800 resize-none text-sm"
-        />
-        <button className="h-full bg-yellow-500 rounded-md px-4 py-2 text-slate-950 font-bold">
-          <PlusCircledIcon />
-        </button>
-      </div>
-
-      <div className="px-4 flex justify-center">
-        <div className="max-w-7xl flex justify-between items-center gap-4 w-96 py-4">
-          <button className="font-bold text-xs bg-yellow-500 px-4 py-2 rounded-md text-slate-950">
-            SHOPPING
-          </button>
-          <button className="font-bold text-xs text-yellow-500">NOTES</button>
-          <button className="font-bold text-xs text-yellow-500">
-            NOTIFICATIONS
+    <footer className="w-screen bg-slate-900 border-slate-800 border-t fixed bottom-0 px-4 flex justify-center">
+      <div className="w-full max-w-7xl flex flex-col sm:w-[450px]">
+        <div className="py-2 h-16 flex gap-2">
+          <textarea
+            placeholder="Enter your text..."
+            className="w-full h-full bg-slate-800 p-2 rounded-md border-transparent border focus:outline-none focus:border focus:border-slate-800 resize-none text-sm"
+          />
+          <button className="h-full bg-yellow-500 rounded-md px-4 py-2 text-slate-950 font-bold flex items-center gap-1.5">
+            <PlusCircledIcon />
+            <div className="text-sm hidden sm:block">Create</div>
           </button>
         </div>
+
+        <div className="flex items-center justify-around sm:justify-center gap-4 py-4 w-full cursor-pointer">
+          {menu.map((item) => {
+            return (
+              <div
+                key={item.title}
+                className="flex items-center p-3 rounded-md bg-slate-800"
+              >
+                <button className="font-bold text-xs text-yellow-500">
+                  {item.icon}
+                </button>
+                <div className="text-sm font-bold text-yellow-500 ml-2 hidden sm:block">
+                  {item.title.toUpperCase()}
+                </div>
+                {item?.bagde && (
+                  <div
+                    className="font-bold w-4 h-4 bg-red-700 flex items-center justify-center rounded-full ml-2"
+                    style={{ fontSize: 8 }}
+                  >
+                    {item.bagde}
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </footer>
   );
 }
