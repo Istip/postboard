5f4b7599382cc21102c6f3d3326c4294ca13cd01
Istip <isticsek@gmail.com>
2023-05-27 15:47:28 +0300

    update signup functionality to create data inside users collection

--- src/utils/signUp.ts
+++ src/utils/signUp.ts
@@ -1,14 +1,24 @@
-import { auth } from "./firebase";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { auth, db } from "./firebase"; // Assuming you have initialized the Firebase app
 
 export default async function signUp() {
   const provider = new GoogleAuthProvider();
 
-  await signInWithPopup(auth, provider)
-    .then((result) => {
-      console.log(result);
-    })
-    .catch((error: Error) => {
-      console.log(error.message);
+  try {
+    const result = await signInWithPopup(auth, provider);
+
+    // Access user information from the result
+    const { user } = result;
+    const { uid, displayName, email, photoURL } = user;
+
+    // Create or replace the user document in Firestore
+    await setDoc(doc(db, "users", uid), {
+      displayName: displayName || "",
+      email: email || "",
+      photoURL: photoURL || "",
     });
+  } catch (error) {
+    console.log(error);
+  }
 }
