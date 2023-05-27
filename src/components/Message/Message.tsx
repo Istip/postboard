c5ae2bc34c24749030db0be610e273b9d42397d9
Istip <isticsek@gmail.com>
2023-05-27 13:35:55 +0300

    update Message component with icons

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -1,3 +1,9 @@
+import {
+  CheckCircledIcon,
+  CrossCircledIcon,
+  InfoCircledIcon,
+} from "@radix-ui/react-icons";
+
 const Message: React.FC<{
   children: React.ReactNode;
   type?: "warning" | "error" | "success";
@@ -7,24 +13,28 @@ const Message: React.FC<{
       bg: "bg-amber-200",
       border: "border-amber-500",
       text: "text-amber-900",
+      icon: <InfoCircledIcon />,
     },
     error: {
       bg: "bg-red-300",
       border: "border-red-500",
       text: "text-red-900",
+      icon: <CrossCircledIcon />,
     },
     success: {
       bg: "bg-green-300",
       border: "border-green-500",
       text: "text-green-900",
+      icon: <CheckCircledIcon />,
     },
   };
 
   return (
     <div
-      className={`w-full rounded-md text-center font-semibold text-sm p-4 border text-opacity-75 ${tokens[type].text} ${tokens[type].bg} ${tokens[type].border}`}
+      className={`w-full rounded-md flex font-semibold text-sm p-4 border text-opacity-75 ${tokens[type].text} ${tokens[type].bg} ${tokens[type].border}`}
     >
-      {children}
+      <div className="pr-4 pt-[3px]">{tokens[type].icon}</div>
+      <div className="w-full">{children}</div>
     </div>
   );
 };
