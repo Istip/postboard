e17a4b665aea4c2de19d0a5d8fe932de39a245a4
Istip <isticsek@gmail.com>
2023-05-27 21:39:22 +0300

    update styling for comment component

--- src/components/Comments/Comment.tsx
+++ src/components/Comments/Comment.tsx
@@ -2,25 +2,53 @@ import { useState } from "react";
 import { Post } from "@/interfaces/Post";
 import Image from "next/image";
 import { ArrowLeftIcon, TrashIcon } from "@radix-ui/react-icons";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "@/utils/firebase";
+import { toast } from "react-hot-toast";
+import { useAuthContext } from "@/context/AuthContext";
 
 const Comment: React.FC<{ post: Post | undefined; comment: any }> = ({
   post,
   comment,
 }) => {
   const [overlap, setOverlap] = useState(false);
+  const { user } = useAuthContext();
+  const commentId = comment.id || "";
+
+  const handleDelete = () => {
+    deleteDoc(doc(db, "comments", commentId))
+      .then(() => {
+        setOverlap(false);
+        toast.success(() => (
+          <div>
+            <span className="font-bold text-md items-center">{`${comment?.text}`}</span>{" "}
+            <span className="opacity-75">comment has been removed!</span>
+          </div>
+        ));
+      })
+      .catch(() => {
+        toast.error("Something went wrong! Please try again!");
+      });
+  };
 
   if (overlap) {
     return (
-      <div className="w-100 left-0 right-0 flex gap-2 bg-slate-800 text-xs p-2 rounded-md font-light">
-        <button className="w-full bg-red-500 px-2 py-1 rounded-l flex items-center gap-1 justify-center">
-          <TrashIcon /> DELETE
-        </button>
-        <button
-          className="w-full bg-slate-600 px-2 py-1 rounded-r flex items-center gap-1 justify-center"
-          onClick={() => setOverlap(false)}
-        >
-          <ArrowLeftIcon /> CANCEL
-        </button>
+      <div className="w-100 mt-4 left-0 right-0 flex flex-col gap-2 bg-slate-800 text-xs p-2 rounded-md font-light">
+        <div className="mb-1">{comment?.text}</div>
+        <div className="flex gap-2">
+          <button
+            onClick={handleDelete}
+            className="w-full bg-red-500 px-2 py-1 rounded-l flex items-center gap-1 justify-center"
+          >
+            <TrashIcon /> DELETE
+          </button>
+          <button
+            className="w-full bg-slate-700 px-2 py-1 rounded-r flex items-center gap-1 justify-center"
+            onClick={() => setOverlap(false)}
+          >
+            <ArrowLeftIcon /> CANCEL
+          </button>
+        </div>
       </div>
     );
   }
