7ba36949e07d7c11d50c30288cb82a66cb7ca66e
Istip <isticsek@gmail.com>
2023-05-29 19:43:14 +0300

    separate commentoverlap to a different component

--- src/components/Comments/Comment.tsx
+++ src/components/Comments/Comment.tsx
@@ -6,6 +6,7 @@ import { deleteDoc, doc } from "firebase/firestore";
 import { db } from "@/utils/firebase";
 import { toast } from "react-hot-toast";
 import { formatDateToYYYYMMDD } from "@/utils/formatDate";
+import CommentOverlap from "./CommentOverlap";
 
 const Comment: React.FC<{ post: Post | undefined; comment: any }> = ({
   post,
@@ -34,32 +35,12 @@ const Comment: React.FC<{ post: Post | undefined; comment: any }> = ({
 
   if (overlap) {
     return (
-      <div className="w-100 mt-4 left-0 right-0 flex flex-col bg-slate-800 text-xs p-2 rounded-md font-light">
-        <div className="flex gap-2 mb-2">
-          <Image
-            src={post?.photoUrl || "/avatar.bmp"}
-            className="w-4 h-4 border rounded-full bg-slate-950 border-white"
-            width={24}
-            height={24}
-            alt={post?.displayName || ""}
-          />
-          <div className="mb-1">{comment?.text}</div>
-        </div>
-        <div className="flex gap-2">
-          <button
-            onClick={handleDelete}
-            className="w-full bg-red-500 px-2 py-1 rounded-l flex items-center gap-1 justify-center"
-          >
-            <TrashIcon /> DELETE
-          </button>
-          <button
-            className="w-full bg-slate-700 px-2 py-1 rounded-r flex items-center gap-1 justify-center"
-            onClick={() => setOverlap(false)}
-          >
-            <ArrowLeftIcon /> CANCEL
-          </button>
-        </div>
-      </div>
+      <CommentOverlap
+        comment={comment}
+        post={post}
+        handleDelete={handleDelete}
+        setOverlap={setOverlap}
+      />
     );
   }
 
---
+++ src/components/Comments/CommentOverlap.tsx
@@ -0,0 +1,48 @@
+import { Post } from "@/interfaces/Post";
+import { ArrowLeftIcon, TrashIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
+
+interface CommentOverlapInterface {
+  // TODO: update type when comment type is ready
+  comment: any;
+  post: Post | undefined;
+  handleDelete: () => void;
+  setOverlap: (value: React.SetStateAction<boolean>) => void;
+}
+
+const CommentOverlap: React.FC<CommentOverlapInterface> = ({
+  comment,
+  post,
+  handleDelete,
+  setOverlap,
+}) => {
+  return (
+    <div className="w-100 mt-4 left-0 right-0 flex flex-col bg-slate-800 text-xs p-2 rounded-md font-light">
+      <div className="flex gap-2 mb-2">
+        <Image
+          src={post?.photoUrl || "/avatar.bmp"}
+          className="w-4 h-4 border rounded-full bg-slate-950 border-white"
+          width={24}
+          height={24}
+          alt={post?.displayName || ""}
+        />
+        <div className="mb-1">{comment?.text}</div>
+      </div>
+      <div className="flex gap-2">
+        <button
+          onClick={handleDelete}
+          className="w-full bg-red-500 px-2 py-1 rounded-l flex items-center gap-1 justify-center"
+        >
+          <TrashIcon /> DELETE
+        </button>
+        <button
+          className="w-full bg-slate-700 px-2 py-1 rounded-r flex items-center gap-1 justify-center"
+          onClick={() => setOverlap(false)}
+        >
+          <ArrowLeftIcon /> CANCEL
+        </button>
+      </div>
+    </div>
+  );
+};
+export default CommentOverlap;
