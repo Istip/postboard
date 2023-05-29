db2370bf17be30abb6d1df7b5ea18cf9e91ae31b
Istip <isticsek@gmail.com>
2023-05-29 17:29:15 +0300

    update notes page

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,3 +1,52 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { db } from "@/utils/firebase";
+import { Post } from "@/interfaces/Post";
+import Loading from "@/components/Loading/Loading";
+import Message from "@/components/Message/Message";
+import Card from "@/components/Card/Card";
+import Toaster from "@/components/Toaster/Toaster";
+
 export default function Notes() {
-  return <div>Notes</div>;
+  const [notes, setNotes] = useState<Post[] | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "posts"), where("type", "==", "notes")),
+      (snapshot) => {
+        const data: any[] = [];
+
+        snapshot.forEach((doc) => {
+          data.push({ ...doc.data(), id: doc.id });
+        });
+
+        setNotes(data);
+      }
+    );
+
+    return () => unsubscribe();
+  }, []);
+
+  if (!notes) {
+    return <Loading title="Loading notes" />;
+  }
+
+  return (
+    <>
+      <Toaster />
+      <main>
+        {!notes?.length && (
+          <Message type="warning">
+            You have nothing on yout shopping list
+          </Message>
+        )}
+
+        {notes.map((note) => (
+          <Card key={note.id} post={note} />
+        ))}
+      </main>
+    </>
+  );
 }
