028ba2f93b5ccbe658959c67ef8bfea0fe4fcca7
Istip <isticsek@gmail.com>
2023-05-29 17:29:10 +0300

    update shopping page

--- src/app/shopping/page.tsx
+++ src/app/shopping/page.tsx
@@ -1,27 +1,30 @@
 "use client";
 
-import { Toaster } from "react-hot-toast";
 import { useEffect, useState } from "react";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
 import { db } from "@/utils/firebase";
 import { Post } from "@/interfaces/Post";
 import Card from "@/components/Card/Card";
 import Loading from "@/components/Loading/Loading";
 import Message from "@/components/Message/Message";
+import Toaster from "@/components/Toaster/Toaster";
 
 export default function Shopping() {
   const [posts, setPosts] = useState<Post[] | null>(null);
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(collection(db, "posts"), (snapshot) => {
-      const data: any[] = [];
+    const unsubscribe = onSnapshot(
+      query(collection(db, "posts"), where("type", "==", "shopping")),
+      (snapshot) => {
+        const data: any[] = [];
 
-      snapshot.forEach((doc) => {
-        data.push({ ...doc.data(), id: doc.id });
-      });
+        snapshot.forEach((doc) => {
+          data.push({ ...doc.data(), id: doc.id });
+        });
 
-      setPosts(data);
-    });
+        setPosts(data);
+      }
+    );
 
     return () => unsubscribe();
   }, []);
@@ -32,36 +35,7 @@ export default function Shopping() {
 
   return (
     <>
-      <Toaster
-        toastOptions={{
-          duration: 3000,
-          success: {
-            style: {
-              background: "#16a34a",
-              color: "white",
-              fontSize: 12,
-            },
-          },
-          error: {
-            style: {
-              background: "#ef4444",
-              color: "white",
-              fontSize: 12,
-            },
-          },
-          loading: {
-            style: {
-              background: "#141c30",
-              color: "white",
-              fontSize: 12,
-              border: "1px solid #1e293b",
-            },
-          },
-          style: {
-            width: "100%",
-          },
-        }}
-      />
+      <Toaster />
       <main>
         {!posts?.length && (
           <Message type="warning">
