cf8defc072d1c1a5f2aae84eba8d43ac5d542ef9
Istip <isticsek@gmail.com>
2023-05-29 19:36:14 +0300

    create formatdate util function

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,16 @@
+export const formatDateToYYYYMMDD = (seconds: number) => {
+  const d = new Date(seconds);
+
+  let date: number | string = d.getDate();
+  let month: number | string = d.getMonth() + 1;
+  let year: number | string = d.getFullYear();
+
+  if (date < 10) {
+    date = "0" + date;
+  }
+  if (month < 10) {
+    month = "0" + month;
+  }
+
+  return `${year}.${month}.${date}`;
+};
