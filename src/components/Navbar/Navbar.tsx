0072940495d69db6952291f248d485cc7f61d400
Istip <isticsek@gmail.com>
2023-05-30 20:38:27 +0300

    update navbar look

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useAuthContext } from "@/context/AuthContext";
 import { getAuth, signOut } from "firebase/auth";
-import { ExitIcon } from "@radix-ui/react-icons";
+import { ExitIcon, ReaderIcon } from "@radix-ui/react-icons";
 
 export default function Navbar() {
   const { user } = useAuthContext();
@@ -30,15 +30,18 @@ export default function Navbar() {
     <nav className="w-screen h-12 bg-slate-900 border-b border-slate-800 px-4 fixed flex justify-center">
       <div className="w-full max-w-7xl flex justify-between items-center gap-4">
         <div>
-          <Link href="/" className="font-bold text-xs text-yellow-500">
-            POSTBOARD
+          <Link
+            href="/"
+            className="font-bold text-sm text-yellow-500 flex gap-1 items-center"
+          >
+            <ReaderIcon /> POSTBOARD
           </Link>
         </div>
         <div>
           {user && (
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-2">
               <button
-                className="text-xs font-bold px-4 py-2 rounded-md bg-yellow-500 text-slate-950 hover:bg-yellow-600 transition-all"
+                className="text-xs font-bold px-4 py-1 rounded-md bg-yellow-500 text-slate-950 hover:bg-yellow-600 transition-all"
                 onClick={handleSignOut}
               >
                 <div className="flex items-center">
@@ -52,8 +55,8 @@ export default function Navbar() {
                 <Image
                   className="rounded-full text-xs border border-white"
                   src={user.photoURL ?? "/default_avatar.bmp"}
-                  width={32}
-                  height={32}
+                  width={24}
+                  height={24}
                   alt={user.displayName ?? "User"}
                 />
               </div>
