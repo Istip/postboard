38cc53f82b1a323b4a5b6ea8a3ba9b930552f470
Istip <isticsek@gmail.com>
2023-06-09 17:15:22 +0300

    fix z-index bug for navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="w-screen h-12 bg-slate-900 border-b border-slate-800 px-4 fixed flex justify-center">
+    <nav className="w-screen h-12 bg-slate-900 border-b border-slate-800 px-4 fixed z-10 flex justify-center">
       <div className="w-full max-w-7xl flex justify-between items-center gap-4">
         <div>
           <Link
