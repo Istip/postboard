bb7f7980e5c719365abb2b4f06f58116047de70d
Istip <isticsek@gmail.com>
2023-06-10 21:04:02 +0300

    create notifications page

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -1,3 +1,47 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { db } from "@/utils/firebase";
+import { useAuthContext } from "@/context/AuthContext";
+import { NotificationType } from "@/interfaces/Notification";
+import Loading from "@/components/Loading/Loading";
+import Notification from "@/components/Notification/Notification";
+
 export default function Notifications() {
-  return <div>Notifications</div>;
+  const [notifications, setNotifications] = useState<NotificationType[]>([]);
+
+  const { user } = useAuthContext();
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      // TODO: update where function to exclude your email
+      // where("email", "!=", user?.email)
+
+      query(collection(db, "notifications"), where("email", "!=", "shopping")),
+      (snapshot) => {
+        const data: any[] = [];
+
+        snapshot.forEach((doc) => {
+          data.push({ ...doc.data(), id: doc.id });
+        });
+
+        setNotifications(data);
+      }
+    );
+
+    return () => unsubscribe();
+  }, []);
+
+  if (!notifications.length) {
+    return <Loading title="Loading your notifications" />;
+  }
+
+  return (
+    <div>
+      {notifications.map((notification, i) => (
+        <Notification key={i} notification={notification} />
+      ))}
+    </div>
+  );
 }
