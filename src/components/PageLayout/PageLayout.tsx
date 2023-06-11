9b5ae2abf269d76366385c97f94d055b11224771
Istip <isticsek@gmail.com>
2023-06-11 11:23:00 +0300

    fix page layout for notification page

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+"use client";
+
+import { usePathname } from "next/navigation";
 
 const PageLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const pathname = usePathname();
+
+  const paddingBottom = pathname === "/notifications" ? "pb-20" : "pb-36";
+
   return (
-    <section className="px-4 pt-16 pb-36 container m-auto max-w-7xl lg:px-2">
+    <section
+      className={`px-4 pt-16 ${paddingBottom} container m-auto max-w-7xl lg:px-2`}
+    >
       <div className="">{children}</div>
     </section>
   );
