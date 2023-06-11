00312e160007574a7219e3158f1432374e6d09bb
Istip <isticsek@gmail.com>
2023-06-11 11:53:51 +0300

    create separate message component for notifications

---
+++ src/components/Notification/NotificationMessage.tsx
@@ -0,0 +1,48 @@
+import { NotificationType } from "@/interfaces/Notification";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+
+export const NotificationMessage: React.FC<{
+  notification: NotificationType;
+}> = ({ notification }) => {
+  const { type } = notification;
+
+  if (type === "comment") {
+    return (
+      <>
+        <span className="font-bold text-yellow-500">
+          {notification.displayName}{" "}
+        </span>
+        commented on:{" "}
+        <span className="font-bold text-white">{notification.post} </span>{" "}
+        <ArrowRightIcon className="inline-flex" />{" "}
+        <span className="font-bold text-white">{notification.text}</span>.
+      </>
+    );
+  }
+
+  if (type === "shopping") {
+    return (
+      <>
+        <span className="font-bold text-yellow-500">
+          {notification.displayName}{" "}
+        </span>{" "}
+        added a new item to the shopping list:{" "}
+        <span className="font-bold text-white">{notification.text}</span>.
+      </>
+    );
+  }
+
+  if (type === "notes") {
+    return (
+      <>
+        <span className="font-bold text-yellow-500">
+          {notification.displayName}{" "}
+        </span>{" "}
+        left a new note:{" "}
+        <span className="font-bold text-white">{notification.text}</span>.
+      </>
+    );
+  }
+
+  return null;
+};
