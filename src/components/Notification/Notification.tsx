7bb3c033bb97837d2565b33c6f190150ae74cb25
Istip <isticsek@gmail.com>
2023-06-11 12:45:14 +0300

    add remove and mark funcionalities to the Notification compo

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,14 +1,35 @@
 import { NotificationType } from "@/interfaces/Notification";
-import { CalendarIcon } from "@radix-ui/react-icons";
+import { CalendarIcon, CheckIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
-import NotificationIcon from "./NotificationIcon";
-import { NotificationMessage } from "./NotificationMessage";
 import { convertTimestamp } from "@/utils/formatDate";
 import { borderColor } from "./notificationUtils";
+import { deleteDoc, doc, setDoc } from "firebase/firestore";
+import { db } from "@/utils/firebase";
+import NotificationIcon from "./NotificationIcon";
+import NotificationMessage from "./NotificationMessage";
+import { toast } from "react-hot-toast";
 
 const Notification: React.FC<{ notification: NotificationType }> = ({
   notification,
 }) => {
+  const handleStatus = () => {
+    setDoc(doc(db, "notifications", notification.id), {
+      ...notification,
+      seen: true,
+    });
+  };
+
+  const handleRemove = async () => {
+    try {
+      // Delete the post
+      await deleteDoc(doc(db, "notifications", notification.id));
+
+      toast.error("Notification removed!");
+    } catch (error) {
+      toast.error("Something went wrong! Please try again!");
+    }
+  };
+
   return (
     <div
       className={`mb-3 text-slate-300 w-full bg-slate-900 p-2 text-xs rounded-l-md flex border-r-2 ${borderColor(
@@ -31,9 +52,18 @@ const Notification: React.FC<{ notification: NotificationType }> = ({
             <CalendarIcon /> {convertTimestamp(notification?.createdAt)}
           </div>
 
-          <div className="w-full flex gap-1 justify-between bg-slate-800 mt-1 py-1 px-2 rounded-md">
-            <div className="w-full text-center">Remove</div>
-            <div className="w-full text-center">Mark as read</div>
+          <div className="w-full flex gap-1 justify-between bg-slate-800 mt-2 py-1 px-2 rounded-md">
+            <button className="w-full text-center" onClick={handleRemove}>
+              Remove
+            </button>
+            <button
+              className="w-full text-center disabled:opacity-50 flex items-center justify-center gap-1"
+              onClick={handleStatus}
+              disabled={notification.seen}
+            >
+              {notification.seen && <CheckIcon />}
+              {notification.seen ? "Seen" : "Mark as seen"}
+            </button>
           </div>
         </div>
 
