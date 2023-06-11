36f7914eb9184821d332e42fe911465180e28922
Istip <isticsek@gmail.com>
2023-06-11 13:13:05 +0300

    update notification

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -8,10 +8,14 @@ import { db } from "@/utils/firebase";
 import NotificationIcon from "./NotificationIcon";
 import NotificationMessage from "./NotificationMessage";
 import { toast } from "react-hot-toast";
+import { useAuthContext } from "@/context/AuthContext";
 
 const Notification: React.FC<{ notification: NotificationType }> = ({
   notification,
 }) => {
+  const { user } = useAuthContext();
+  const isCreator = user?.email === notification.email;
+
   const handleStatus = () => {
     if (notification.id) {
       setDoc(doc(db, "notifications", notification.id), {
@@ -23,7 +27,6 @@ const Notification: React.FC<{ notification: NotificationType }> = ({
 
   const handleRemove = async () => {
     try {
-      // Delete the post
       if (notification.id) {
         await deleteDoc(doc(db, "notifications", notification.id));
       }
@@ -54,19 +57,21 @@ const Notification: React.FC<{ notification: NotificationType }> = ({
             <CalendarIcon /> {convertTimestamp(notification?.createdAt)}
           </div>
 
-          <div className="w-full flex gap-1 justify-between bg-slate-800 mt-2 py-1 px-2 rounded-md">
-            <button className="w-full text-center" onClick={handleRemove}>
-              Remove
-            </button>
-            <button
-              className="w-full text-center disabled:opacity-50 flex items-center justify-center gap-1"
-              onClick={handleStatus}
-              disabled={notification.seen}
-            >
-              {notification.seen && <CheckIcon />}
-              {notification.seen ? "Seen" : "Mark as seen"}
-            </button>
-          </div>
+          {!isCreator && (
+            <div className="w-full flex gap-1 justify-between bg-slate-800 mt-2 py-1 px-2 rounded-md">
+              <button className="w-full text-center" onClick={handleRemove}>
+                Remove
+              </button>
+              <button
+                className="w-full text-center disabled:opacity-50 flex items-center justify-center gap-1"
+                onClick={handleStatus}
+                disabled={notification.seen}
+              >
+                {notification.seen && <CheckIcon />}
+                {notification.seen ? "Seen" : "Mark as seen"}
+              </button>
+            </div>
+          )}
         </div>
 
         <NotificationIcon notification={notification} />
