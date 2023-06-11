72effe56c9f287e126d5670062ec72ad6c9dbeb6
Istip <isticsek@gmail.com>
2023-06-11 11:53:37 +0300

    create separate icon component for notifications

---
+++ src/components/Notification/NotificationIcon.tsx
@@ -0,0 +1,31 @@
+import { NotificationType } from "@/interfaces/Notification";
+import {
+  ChatBubbleIcon,
+  FileIcon,
+  ListBulletIcon,
+} from "@radix-ui/react-icons";
+
+const NotificationIcon: React.FC<{ notification: NotificationType }> = ({
+  notification,
+}) => {
+  return (
+    <div className="h-full flex items-center">
+      {notification.type === "shopping" && (
+        <div className="bg-green-500 m-1 p-1.5 flex items-center rounded-full text-black">
+          <ListBulletIcon />
+        </div>
+      )}
+      {notification.type === "notes" && (
+        <div className="bg-yellow-500 m-1 p-1.5 flex items-center rounded-full text-black">
+          <FileIcon />
+        </div>
+      )}
+      {notification.type === "comment" && (
+        <div className="bg-slate-500 m-1 p-1.5 flex items-center rounded-full">
+          <ChatBubbleIcon />
+        </div>
+      )}
+    </div>
+  );
+};
+export default NotificationIcon;
