ffd8a4de8be852e7d20249e4dd8373f027872da0
Istip <isticsek@gmail.com>
2023-06-11 12:48:41 +0300

    remove menu data to separate file

---
+++ src/components/Footer/footerData.tsx
@@ -0,0 +1,19 @@
+import { BellIcon, FileIcon, ListBulletIcon } from "@radix-ui/react-icons";
+
+export const footerMenu = [
+  {
+    title: "shopping",
+    icon: <ListBulletIcon />,
+    bagde: null,
+  },
+  {
+    title: "notes",
+    icon: <FileIcon />,
+    bagde: null,
+  },
+  {
+    title: "notifications",
+    icon: <BellIcon />,
+    bagde: 10,
+  },
+];
