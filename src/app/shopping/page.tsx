5a8b38c2253d22bebe60df58c0fbdf507768784e
Istip <isticsek@gmail.com>
2023-07-05 20:30:16 +0300

    add filter function for shopping list

--- src/app/shopping/page.tsx
+++ src/app/shopping/page.tsx
@@ -15,9 +15,11 @@ import Card from "@/components/Card/Card";
 import Loading from "@/components/Loading/Loading";
 import Message from "@/components/Message/Message";
 import Toaster from "@/components/Toaster/Toaster";
+import { BookmarkFilledIcon } from "@radix-ui/react-icons";
 
 export default function Shopping() {
   const [posts, setPosts] = useState<Post[]>([]);
+  const [filter, setFilter] = useState(false);
 
   useEffect(() => {
     const unsubscribe = onSnapshot(
@@ -54,19 +56,47 @@ export default function Shopping() {
           </Message>
         )}
 
-        <AnimatePresence>
-          {posts.map((post) => (
-            <motion.div
-              key={post.id}
-              initial={{ opacity: 0, height: 0 }}
-              animate={{ opacity: 1, height: "auto" }}
-              exit={{ opacity: 0, height: 0 }}
+        <div className="w-100 flex items-center justify-center gap-1 mb-2 text-sm">
+          <div className="flex">
+            <button
+              type="button"
+              onClick={() => setFilter(false)}
+              className={`px-4 py-2 ${
+                !filter ? "bg-yellow-500" : "bg-slate-700"
+              } rounded-l-md`}
             >
-              <div className="py-0.5">
-                <Card post={post} comments={false} />
-              </div>
-            </motion.div>
-          ))}
+              Show all
+            </button>
+            <button
+              type="button"
+              onClick={() => setFilter(true)}
+              className={`px-4 py-2 flex items-center gap-1 ${
+                filter ? "bg-yellow-500" : "bg-slate-700"
+              } rounded-r-md`}
+            >
+              <BookmarkFilledIcon /> Marked
+            </button>
+          </div>
+        </div>
+
+        <AnimatePresence>
+          {posts
+            .filter((post) => {
+              if (!filter) return true;
+              return post.marked;
+            })
+            .map((post) => (
+              <motion.div
+                key={post.id}
+                initial={{ opacity: 0, height: 0 }}
+                animate={{ opacity: 1, height: "auto" }}
+                exit={{ opacity: 0, height: 0 }}
+              >
+                <div className="py-0.5">
+                  <Card post={post} comments={false} />
+                </div>
+              </motion.div>
+            ))}
         </AnimatePresence>
       </main>
     </>
