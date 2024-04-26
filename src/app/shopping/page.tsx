32cd91212bdabe0505db9d204df03b4390f83d5f
Pasztor Isti <isticsek@gmail.com>
2024-04-26 14:12:00 +0300

    make tabs bigger

--- src/app/shopping/page.tsx
+++ src/app/shopping/page.tsx
@@ -58,11 +58,11 @@ export default function Shopping() {
 
         {posts?.length && (
           <div className="w-100 flex items-center justify-center gap-2 mb-2 text-xs">
-            <div className="flex">
+            <div className="flex w-full">
               <button
                 type="button"
                 onClick={() => setFilter(false)}
-                className={`p-2 rounded-l-md  ${
+                className={`px-2 py-4 rounded-l-md w-full ${
                   !filter
                     ? "bg-yellow-500 text-stone-900"
                     : "bg-stone-700 text-stone-50"
@@ -73,13 +73,16 @@ export default function Shopping() {
               <button
                 type="button"
                 onClick={() => setFilter(true)}
-                className={`p-2 flex items-center gap-1 rounded-r-md ${
+                className={`px-2 py-4 flex items-center justify-center w-full gap-1 rounded-r-md ${
                   filter
                     ? "bg-yellow-500 text-stone-900"
                     : "bg-stone-700 text-stone-50"
                 }`}
               >
-                <BookmarkFilledIcon /> Marked
+                <div>
+                  <BookmarkFilledIcon />
+                </div>{" "}
+                <div>Marked</div>
               </button>
             </div>
           </div>
