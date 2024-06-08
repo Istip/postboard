c045659b639048620a746ece697673f8ffb3e71c
Pasztor Isti <isticsek@gmail.com>
2024-06-08 16:53:24 +0300

    fix footer hint positioning

--- src/components/Footer/FooterHint.tsx
+++ src/components/Footer/FooterHint.tsx
@@ -6,7 +6,7 @@ const FooterHint: React.FC<{
   return (
     <>
       {isShopping && text.length && filteredHints.length ? (
-        <div className="bg-stone-800 border no-scrollbar border-stone-700 fixed w-auto overflow-x-scroll left-2 right-2 rounded-2xl p-2 bottom-36 flex items-center gap-2">
+        <div className="bg-stone-800 border no-scrollbar border-stone-700 fixed w-auto overflow-x-scroll left-2 right-2 rounded-2xl p-2 bottom-32 flex items-center gap-2">
           {filteredHints.length > 1 && (
             <div className="rounded-lg text-xs font-extrabold py-2 px-4 bg-green-600 text-stone-50">
               {filteredHints.length}
@@ -16,7 +16,7 @@ const FooterHint: React.FC<{
           {filteredHints.map((hint) => (
             <div
               key={hint.id}
-              className="rounded-lg text-xs font-light bg-stone-700 py-2 px-4 whitespace-nowrap"
+              className="rounded-xl text-xs font-light bg-stone-700 py-2 px-4 whitespace-nowrap"
             >
               {hint.text.toLowerCase()}
             </div>
