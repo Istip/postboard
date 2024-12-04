6ba490459d337989ed2d00e776879f33d37beccf
Pasztor Isti <isticsek@gmail.com>
2024-12-04 10:54:34 +0200

    add counts to the footer tabs

--- src/components/Footer/FooterMenu.tsx
+++ src/components/Footer/FooterMenu.tsx
@@ -1,7 +1,14 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { FileIcon, ListBulletIcon } from "@radix-ui/react-icons";
+import {
+  collection,
+  getCountFromServer,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "@/utils/firebase";
 
 const footerMenu = [
   {
@@ -16,12 +23,32 @@ const footerMenu = [
 
 const FooterMenu = () => {
   const [current, setCurrent] = useState("/shopping");
+  const [shoppingCount, setShoppingCount] = useState<number | null>(null);
+  const [notesCount, setNotesCount] = useState<number | null>(null);
 
   const pathname = usePathname();
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const col = collection(db, "posts");
+      const shoppingQuery = query(col, where("type", "==", "shopping"));
+      const notesQuery = query(col, where("type", "==", "notes"));
+
+      const shoppingCount = await getCountFromServer(shoppingQuery);
+      const notesCount = await getCountFromServer(notesQuery);
+
+      setShoppingCount(shoppingCount.data().count);
+      setNotesCount(notesCount.data().count);
+    };
+
+    fetchData();
+  }, [shoppingCount, notesCount]);
+
+  const isCountFetched = shoppingCount !== null && notesCount !== null;
+
   return (
     <>
-      {footerMenu.map((item) => {
+      {footerMenu.map((item, i) => {
         return (
           <Link
             onClick={() => current !== item.title && setCurrent(item.title)}
@@ -34,11 +61,19 @@ const FooterMenu = () => {
             } p-4 flex items-center justify-center rounded-xl w-full`}
             style={{ WebkitTapHighlightColor: "transparent" }}
           >
-            <div className="font-bold text-xs">{item.icon}</div>
-            <div className="text-sm font-bold ml-2 hidden sm:block">
-              {item.title.toUpperCase()}
+            <div className="flex items-center justify-center gap-4">
+              <div className="font-bold text-xs">{item.icon}</div>
+              <div className="text-sm font-bold ml-2 hidden sm:block">
+                {item.title.toUpperCase()}
+              </div>
+              <span className="sr-only">{item.title.toUpperCase()}</span>
+
+              <div className="text-[8px] w-4 h-4 flex items-center justify-center opacity-50 font-bold bg-stone-600 rounded-full ">
+                {isCountFetched && (
+                  <span>{i === 0 ? shoppingCount : notesCount}</span>
+                )}
+              </div>
             </div>
-            <span className="sr-only">{item.title.toUpperCase()}</span>
           </Link>
         );
       })}
